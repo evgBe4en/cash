75561edaeee8dd216fb1d64a60dbc26d077f129a
Женя Здебский <crfqg2014@gmail.com>
2022-01-19 10:54:18 +0200

    master

--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -10,7 +10,7 @@ export const TransactionList = () => {
         <div>
          <h3>История</h3>
             <ul className="list">
-                {transactions && transactions.map(transaction => (<Transaction key={transaction.id} transaction={transaction} />))}
+                {transactions.map(transaction => (<Transaction key={transaction.id} transaction={transaction} />))}
                
             </ul>
         </div>
