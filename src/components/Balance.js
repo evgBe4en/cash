eeff42d647ff1a2c34406f6583ba08124499e0b1
Женя Здебский <crfqg2014@gmail.com>
2022-01-19 10:28:02 +0200

    master

--- src/components/Balance.js
+++ src/components/Balance.js
@@ -2,7 +2,7 @@ import React, { useContext} from 'react'
 import { GlobalContext } from '../context/GlobalState'
 
 
-export const Balance = (props) => {
+export const Balance = () => {
     const {transactions} = useContext(GlobalContext)
 
     const amounts = transactions.map(transaction => transaction.amount);
